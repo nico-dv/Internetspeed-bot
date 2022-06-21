26f28f15b16f002ec6727c47cfcbdd34543f0ea3
nico-dv <vv.nicov@gmail.com>
2022-06-21 15:57:15 +0100

    finished get_internet_speed method

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
+
+
 from constants import *
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 import time
 
 
@@ -14,8 +17,22 @@ class InternetSpeedBot:
 
     def get_internet_speed(self):
         self.driver.get("https://www.speedtest.net/")
-        time.sleep(5)
+
+        time.sleep(3)
+
+        accept_button = self.driver.find_element(By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')
+        accept_button.click()
+        # cookies popup
+        time.sleep(10)
+
+        go_button = self.driver.find_element(By.XPATH, '/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
+        go_button.click()
+        time.sleep(55)
+        download = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        upload = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
+
+        if int(download) > PROMISED_DOWN and int(upload) > PROMISED_UP:
+            exit()
 
     def tweet_at_provider(self):
         pass
-
