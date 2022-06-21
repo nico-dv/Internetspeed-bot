fbf83d4019bbaa74563eea2d35983586d127c08f
nico-dv <vv.nicov@gmail.com>
2022-06-21 17:30:17 +0100

    added twitter posting method

--- bot.py
+++ bot.py
@@ -11,6 +11,8 @@ import time
 
 class InternetSpeedBot:
     def __init__(self):
+        self.upload = 0
+        self.download = 0
         self.options = Options()
         self.options.add_argument("start-maximized")
         self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.options)
@@ -30,9 +32,47 @@ class InternetSpeedBot:
         time.sleep(55)
         download = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
         upload = self.driver.find_element(By.XPATH, '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
-
-        if int(download) > PROMISED_DOWN and int(upload) > PROMISED_UP:
+        self.download = download
+        self.upload = upload
+        if float(download) > PROMISED_DOWN and float(upload) > PROMISED_UP:
             exit()
 
     def tweet_at_provider(self):
-        pass
+        self.driver.get("https://twitter.com/i/flow/login")
+        time.sleep(3)
+
+        email = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input')
+        email.send_keys(TWITTER_EMAIL)
+        time.sleep(3)
+
+        next_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div')
+        next_button.click()
+        time.sleep(3)
+
+        # solve the unusual login activity issue
+
+        phone_number = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input')
+        phone_number.send_keys(PHONE)
+        time.sleep(3)
+
+        pn_next = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div/div/div/div')
+        pn_next.click()
+        time.sleep(3)
+
+        password = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+        password.send_keys(TWITTER_PASSWORD)
+        time.sleep(3)
+
+        login_button = self.driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div/div')
+        login_button.click()
+
+        time.sleep(3)
+
+        tweet_box = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div')
+        tweet = f"Hey @SkyUK, why is my download speed {self.download} Mbps, if my minimum guaranteed speed is {PROMISED_DOWN} Mbps?"
+        tweet_box.send_keys(tweet)
+
+        tweet_button = self.driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div/div/span/span')
+        tweet_button.click()
+
+        time.sleep(150)
